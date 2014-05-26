4c0db45a621bb3944b0491c11ebf9dc4619ddabb
Reed Sweeney <reed@Reeds-MacBook-Pro.local>
2014-05-26 13:31:15 -0700

    ability to add and delete stashes

--- Stash/SHStash+Manage.h
+++ Stash/SHStash+Manage.h
@@ -8,6 +8,12 @@
 
 #import "SHStash.h"
 
+typedef void(^SHStashCompletionHandler)(NSError *error);
+
 @interface SHStash (Manage)
+
++ (void)stashWithTitle:(NSString *)title text:(NSString *)text origin:(BOOL)isMine completion:(SHStashCompletionHandler)completionHandler;
++ (void)deleteStash:(SHStash *)stash completion:(SHStashCompletionHandler)completionHandler;
++ (void)editStash:(SHStash *)stash title:(NSString *)title text:(NSString *)text comletion:(SHStashCompletionHandler)completionHandler;
 
 @end
