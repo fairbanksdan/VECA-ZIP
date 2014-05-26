1ef7bc059648315bc355a31571daf29c7fe3fef6
Taylor Potter <taylorkpotter@me.com>
2014-05-26 09:39:09 -0700

    Initial Commit

---
+++ Stash/SHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHAppDelegate.h
+//  Stash
+//
+//  Created by Taylor Potter on 5/26/14.
+//  Copyright (c) 2014 stash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Stash/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Stash
+//
+//  Created by Taylor Potter on 5/26/14.
+//  Copyright (c) 2014 stash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([SHAppDelegate class]));
+  }
+}
