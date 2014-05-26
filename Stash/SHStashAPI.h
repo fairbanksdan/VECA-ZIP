aff4c0a39e752090d5055e2801e50f0efafe32d0
Michael Babiy <ic.babiy@gmail.com>
2014-05-26 09:57:45 -0700

    Add SHStashAPI classes.

---
+++ Stash/SHStashAPI.h
@@ -0,0 +1,13 @@
+//
+//  SHStashAPI.h
+//  Stash
+//
+//  Created by iC on 5/26/14.
+//  Copyright (c) 2014 stash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHStashAPI : NSObject
+
+@end
---
+++ Stash/SHStashAPI.m
@@ -0,0 +1,13 @@
+//
+//  SHStashAPI.m
+//  Stash
+//
+//  Created by iC on 5/26/14.
+//  Copyright (c) 2014 stash. All rights reserved.
+//
+
+#import "SHStashAPI.h"
+
+@implementation SHStashAPI
+
+@end
