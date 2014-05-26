6932f0607b059e3f74af8b24eb4436d9b0bef8c9
Reed Sweeney <reed@Reeds-MacBook-Pro.local>
2014-05-26 13:31:09 -0700

    ability to add and delete stashes

---
+++ Stash/SHStash+Manage.h
@@ -0,0 +1,13 @@
+//
+//  SHStash+Manage.h
+//  Stash
+//
+//  Created by Reed Sweeney on 5/26/14.
+//  Copyright (c) 2014 stash. All rights reserved.
+//
+
+#import "SHStash.h"
+
+@interface SHStash (Manage)
+
+@end
---
+++ Stash/SHStash+Manage.m
@@ -0,0 +1,13 @@
+//
+//  SHStash+Manage.m
+//  Stash
+//
+//  Created by Reed Sweeney on 5/26/14.
+//  Copyright (c) 2014 stash. All rights reserved.
+//
+
+#import "SHStash+Manage.h"
+
+@implementation SHStash (Manage)
+
+@end
