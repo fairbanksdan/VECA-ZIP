e01ce7f5275a3ebb67c2cbbb48ef15dda099d94c
Michael Babiy <ic.babiy@gmail.com>
2014-05-27 13:05:31 -0700

    Add initial implementation for the share option.

--- Stash/SHEditViewController.m
+++ Stash/SHEditViewController.m
@@ -10,6 +10,8 @@
 
 @interface SHEditViewController ()
 
+- (IBAction)shareButtonSelected:(id)sender;
+
 @end
 
 @implementation SHEditViewController
@@ -22,6 +24,28 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Action Methods
+
+- (NSArray *)activityItems
+{
+    NSString *title = @"New Title";
+    NSString *text = @"This is a very cool idea. I love it!";
+    
+    return @[title, text];
+}
+
+- (NSArray *)applicationActivities
+{
+    return nil;
+}
+
+- (IBAction)shareButtonSelected:(id)sender
+{
+    UIActivityViewController *activityViewController = [[UIActivityViewController alloc]initWithActivityItems:[self activityItems]
+                                                                                        applicationActivities:[self applicationActivities]];
+    [self presentViewController:activityViewController animated:YES completion:nil];
 }
 
 @end
