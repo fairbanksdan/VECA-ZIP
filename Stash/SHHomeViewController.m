6fe978e98cd138306ad43bf5135843cd2f163b97
Michael Babiy <ic.babiy@gmail.com>
2014-05-27 12:47:31 -0700

    Remove test code from HomeVC.

--- Stash/SHHomeViewController.m
+++ Stash/SHHomeViewController.m
@@ -26,44 +26,4 @@
     [super didReceiveMemoryWarning];
 }
 
-- (IBAction)create:(id)sender
-{
-    NSString *username = @"stashapp";
-    NSString *apiKey = @"R_15afe52cbf634d5e8192cba22438bea9";
-    NSString *url = [NSString stringWithFormat:@"http://stashapp.com/?id=%@", [[NSUUID UUID]UUIDString]];
-    NSString *requestString = [NSString stringWithFormat:@"http://api.bit.ly/v3/shorten?login=%@&apikey=%@&longUrl=%@&format=txt", username, apiKey, url];
-    NSString *shortURLString = [NSString stringWithContentsOfURL:[NSURL URLWithString:requestString] encoding:NSUTF8StringEncoding error:nil];
-    
-    NSLog(@"%@", shortURLString);
-    
-    
-  [SHStash stashWithTitle:@"App Name" text:@"This app is awesome" origin:YES completion:^(NSError *error) {
-    if (!error) {
-      NSLog(@"Success!!");
-    }
-  }];
-}
-
-- (IBAction)delete:(id)sender
-{
-  NSArray *results = [SHStash findAll];
-  for (SHStash *stash in results) {
-      
-    [SHStash deleteStash:stash completion:^(NSError *error) {
-      NSLog(@"Deleted Stash");
-    }];
-  }
-}
-
-- (IBAction)read:(id)sender
-{
-    [[SHStashAPI sharedAPI]GETRequestForStash:@"sKI1Y9dQrT" completion:^(NSError *error) {
-        
-        if (!error) {
-            NSLog(@"Retrieved from Parse and saved to Core Data.");
-        }
-        
-    }];
-}
-
 @end
