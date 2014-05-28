bd352251ee84e341ea99f4c46e2fe67e927c8b94
Dan Fairbanks <fairbanksdan@gmail.com>
2014-05-28 15:35:38 -0700

    Added lifecycle to each VC

---
+++ Stash/SHCategoryCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SHCategoryCollectionViewCell.h
+//  Stash
+//
+//  Created by Daniel Fairbanks on 5/28/14.
+//  Copyright (c) 2014 stash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHCategoryCollectionViewCell : UICollectionViewCell
+
+@end
