1ec44acb33800d4bacb1c1677a86b690eb0aca64
Dan Fairbanks <fairbanksdan@gmail.com>
2014-05-28 11:43:11 -0700

    Added IBAction to Pitch Hint. Added Pitch Hint VC.

--- Stash/SHAddPitchViewController.m
+++ Stash/SHAddPitchViewController.m
@@ -34,6 +34,37 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)showHint:(id)sender
+{
+    UIViewController *popover = [self.storyboard instantiateViewControllerWithIdentifier:@"PopoverVC"];
+    popover.view.bounds = CGRectMake(0, 0, 260, 300);
+    popover.view.center = self.view.center;
+    popover.view.alpha = 0.f;
+    UIButton *dismissButton = [[UIButton alloc] initWithFrame:popover.view.bounds];
+    
+//    popover.view.center = CGPointMake(self.view.center.x, self.view.frame.size.height);
+    
+    [self.view addSubview:popover.view];
+    [popover.view addSubview:dismissButton];
+    [dismissButton addTarget:self action:@selector(dismissPopover:) forControlEvents:UIControlEventTouchUpInside];
+
+    [UIView animateWithDuration:0.4
+                     animations:^{
+//                         popover.view.center = self.view.center;
+                         popover.view.alpha = 1.f;
+                     }];
+}
+
+- (void)dismissPopover:(id)sender
+{
+    UIView *popoverView = [(UIButton *)sender superview];
+    [UIView animateWithDuration:0.4
+                     animations:^{
+                         popoverView.alpha = 0.f;
+                     } completion:^(BOOL finished) {
+                         [popoverView removeFromSuperview];
+                     }];
+}
 
 /*
 #pragma mark - Navigation
