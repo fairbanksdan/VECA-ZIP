bb5bec10f5517d7fe1b7b72ae82b34c271cdf03d
Michael Babiy <ic.babiy@gmail.com>
2014-05-28 16:01:29 -0700

    Clean up MBStoreCheck class.

--- Stash/MBStoreCheck/MBStoreCheck.m
+++ Stash/MBStoreCheck/MBStoreCheck.m
@@ -41,10 +41,7 @@ static NSString * const kProductIdentifier = @"com.minddiaper.Stash.StashPro";
 {
     if ([SKPaymentQueue canMakePayments]) {
         SKProductsRequest *request = [[SKProductsRequest alloc]initWithProductIdentifiers:[NSSet setWithObject:kProductIdentifier]];
-        request.delegate = self;
-        
-        // Starting the request.
-        [request start];
+        request.delegate = self; [request start];
     } else {
         [[[UIAlertView alloc]initWithTitle:@"Prohibited"
                                    message:@"Parental Control is enabled, cannot make a purchase!"
@@ -62,24 +59,24 @@ static NSString * const kProductIdentifier = @"com.minddiaper.Stash.StashPro";
     for (SKPaymentTransaction *transaction in transactions) {
         switch (transaction.transactionState) {
             case SKPaymentTransactionStatePurchasing:
-                NSLog(@"Processing...");
+                // NSLog(@"Processing...");
                 break;
                 
             case SKPaymentTransactionStatePurchased:
                 [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
                 [[NSUserDefaults standardUserDefaults]setBool:YES forKey:kUserAuthorized];
-                NSLog(@"Purchase complete. Unlock the feature.");
+                // NSLog(@"Purchase complete. Unlock the feature.");
                 break;
                 
             case SKPaymentTransactionStateRestored:
                 [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-                NSLog(@"Previous purchase restored...");
+                // NSLog(@"Previous purchase restored...");
                 break;
                 
             case SKPaymentTransactionStateFailed:
                 if (transaction.error.code != SKErrorPaymentCancelled) { NSLog(@"Error payment cancelled"); }
                 [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-                NSLog(@"Purchase error.");
+                // NSLog(@"Purchase error.");
                 break;
                 
             default:
@@ -91,7 +88,6 @@ static NSString * const kProductIdentifier = @"com.minddiaper.Stash.StashPro";
 - (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response
 {
     if ([response.products count]) {
-        NSLog(@"Retrieved Products: %@", response.products);
         [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
         [[SKPaymentQueue defaultQueue] addPayment:[SKPayment paymentWithProduct:response.products[0]]]; // Transaction happening... cool.
     } else {
